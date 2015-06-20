843d5ef8ad1f557f90cfe9a0f3e47acdf3e183b3
Paulo <paulo.castro48@gmail.com>
2015-06-20 17:26:09 +0800

    Added whitelist

--- demo/assets/php/send.php
+++ demo/assets/php/send.php
@@ -5,6 +5,7 @@ require('Pusher.php');
 $app_id = '75978';
 $app_key = 'c7074daf2062830b98d0';
 $app_secret = 'ea27c6ff5e8972016a0f';
+$dj_ip = '192.168.1.113';
 
 $pusher = new Pusher($app_key, $app_secret, $app_id);
 
@@ -16,4 +17,6 @@ if(isset($_POST['message'])) {
 	$data['localip'] = 'n/a';
 }
 
-$pusher->trigger('test_channel', 'my_event', $data);
+if ( strpos( $data['localip'], $dj_ip ) !== false ) {    
+	$pusher->trigger('test_channel', 'my_event', $data);
+}
